e5339d0b0da60cafc21d5ef1902a0203a5cb30e9
Exonoesis <Blue_Flame_14@hotmail.com>
2020-08-19 20:51:15 -0600

    Implemented Clickable Objects

---
+++ Assets/Scripts/Interactive Viewer/Layouts/ClickerScene/ClickableObjectMethods.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableObjectMethods : MonoBehaviour
+{
+    public void ClickedEevee()
+    {
+        print("Clicked Eevee");
+    }
+
+    public void ClickedGem()
+    {
+        print("Clicked Gem");
+    }
+}
---
+++ Assets/Scripts/Interactive Viewer/Layouts/ClickerScene/Clicked.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+     
+public class Clicked : MonoBehaviour
+{
+    public RectTransform rectTransform;
+    //public MaskableGraphic buttonIcon;
+    public UnityEvent onClicked;
+
+    private Rect rectangle;
+    
+    void Start()
+    {
+        rectTransform = gameObject.GetComponent<RectTransform> ();
+        //buttonIcon = gameObject.GetComponent<MaskableGraphic> ();   
+    }
+
+    void Update()
+    {
+        rectangle = rectTransform.rect;
+        Vector3 mPos = Input.mousePosition;
+
+        if (mPos.x > rectTransform.position.x - (rectangle.width) 
+        && mPos.x < rectTransform.position.x + (rectangle.width)
+        && mPos.y > rectTransform.position.y - (rectangle.height)
+        && mPos.y < rectTransform.position.y + (rectangle.height)
+        && Input.GetMouseButtonUp (0)) 
+        {
+            onClicked.Invoke(); 
+        } 
+    }
+}
