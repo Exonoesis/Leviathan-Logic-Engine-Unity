a2fe801a325bd0cbbb545b0841c1bacfac05f27b
Exonoesis <Blue_Flame_14@hotmail.com>
2021-02-10 15:09:00 -0700

    - Updated MouseListener to use OnMouseOver() and OnMouseExit() as a fix for mouse detection issues

--- Assets/Scripts/Controllers/MouseListener.cs
+++ Assets/Scripts/Controllers/MouseListener.cs
@@ -1,33 +1,26 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MouseListener : MonoBehaviour
 {
-    private PolygonCollider2D objCollider;
     private AssetViewer aViewer;
 
     void Start()
     {
-        objCollider = gameObject.GetComponent<PolygonCollider2D> ();
         aViewer = AssetViewer.Instance;
     }
-
-    void Update()
+    
+    void OnMouseOver()
     {
-        Vector3 mPos = Input.mousePosition;
-
-        if (objCollider.bounds.Contains(mPos))
+        aViewer.Darken(gameObject);
+        
+        if (Input.GetMouseButtonUp(MouseCodes.PrimaryButton))
         {
-            aViewer.Darken(gameObject);
+            aViewer.handleClickedPrefab(transform.parent.gameObject);
+        }
+    }
 
-            if (Input.GetMouseButtonUp(MouseCodes.PrimaryButton))
-            {
-                aViewer.handleClickedPrefab(transform.parent.gameObject);
-            }
-        }
-        else
-        {
-            aViewer.Lighten(gameObject);
-        }
+    void OnMouseExit()
+    {
+        aViewer.Lighten(gameObject);
     }
 }
